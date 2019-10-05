763d317d4c15ec5e8cae41562ecc8b26318a7057
ThinPotato <bryce940@gmail.com>
2019-10-05 15:31:51 -0400

    began integer condition

--- HomeWork3/src/app/Block.java
+++ HomeWork3/src/app/Block.java
@@ -2,6 +2,7 @@ package app;
 
 class Block {
     Variable[] variables = new Variable[10];
+    int location = 0;
 
     Block() {
 
@@ -9,6 +10,11 @@ class Block {
 
     public void addVariable(int location, String name, int data) {
         variables[location] = new Variable(name, data);
+    }
+
+    public void addVariable(String name, int data) {
+        variables[location] = new Variable(name, data);
+        location++;
     }
 
     public void removeVariable(int location) {
