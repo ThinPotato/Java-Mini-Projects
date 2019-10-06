1abd2676a48c20bbada4c0c12d68b2dfff13bb14
ThinPotato <bryce940@gmail.com>
2019-10-06 19:01:00 -0400

    print(), toString(), newVariable(), getAt() added

--- HomeWork3/src/app/Block.java
+++ HomeWork3/src/app/Block.java
@@ -20,4 +20,45 @@ class Block {
     public void removeVariable(int location) {
         variables[location] = null;
     }
+
+    public String toString() {
+        String string = "";
+        String[] name = new String[10];
+        String[] intvalue = new String[10];
+        int count = 0;
+        for (int i = 0; i < variables.length; i++) {
+            if (variables[i] != null) {
+                name[i] = variables[i].getName();
+                intvalue[i] = Integer.toString(variables[i].getIntegerValue());
+                count++;
+            }
+        }
+        for (int j = 0; j < count; j++) {
+            string += name[j] + ":" + intvalue[j] + " ";
+        }
+        return string;
+    }
+
+    public Variable findVarialbe(String name) {
+        for (int i = 0; i < variables.length; i++)
+            if (variables[i] != null && variables[i].getName().equals(name)) {
+                return variables[i];
+            }
+        System.out.println("could not find: " + name);
+        return null;
+    }
+
+    /**
+     * @return the variables
+     */
+    public Variable[] getVariables() {
+        return variables;
+    }
+
+    /**
+     * @return the location
+     */
+    public int getLocation() {
+        return location;
+    }
 }
--- HomeWork3/src/app/BlockTracer.java
+++ HomeWork3/src/app/BlockTracer.java
@@ -16,37 +16,87 @@ public class BlockTracer {
 
         File file = new File("src/app/sample1.c");
         Scanner sc = new Scanner(file);
-        Block block = new Block();
-        ;
-        stack.add(block);
         while (sc.hasNextLine()) {
             currentLine = sc.nextLine();
-            if (currentLine.contains("{")) {
-                System.out.println("{ found");
-            }
-            if (currentLine.contains("int")) {
-                int intIndex = currentLine.indexOf("int ");
-                String[] lineSplit = currentLine.split(" ");
-                for (int i = 0; i < lineSplit.length; i++) {
-                    System.out.println(lineSplit[i]);
-
-                    if (lineSplit[i].equals("=")) {
-                        block.addVariable(lineSplit[i + 1], Integer.parseInt(lineSplit[i + 2]));
-                        if (lineSplit[i + 3].equals(",")) {
-
-                        }
-                    }
+            for (int i = 0; i < currentLine.length(); i++) {
+                if (currentLine.substring(i, i + 1).equals("{")) {
+                    System.err.println("new block made.");
+                    stack.push(new Block());
                 }
-
-                // block.addVariable(location, name, data);
-            }
-            if (currentLine.contains("/*$print")) {
-                System.out.println("print found");
-            }
-            if (currentLine.contains("}")) {
-                System.err.println("} found");
+                if (currentLine.substring(i, i + 1).equals("}")) {
+                    System.err.println("stack popped.");
+                    stack.pop();
+                }
+                if (currentLine.length() >= i + 4 && currentLine.substring(i, i + 4).equals("int ")) {
+                    // TODO: create varialbe in highest block.
+                    newVariable(i);
+                }
+                if (currentLine.length() >= i + 8 && currentLine.substring(i, i + 8).equals("/*$print")) {
+                    System.err.println("attempting to print");
+                    currentLine = currentLine.substring(i);
+                    print();
+                }
             }
         }
         sc.close();
+    }
+
+    public static void print() {
+        currentLine = currentLine.replace("*/", "");
+        currentLine = currentLine.replace("/*$", "");
+        String[] trimmed = currentLine.split(" ");
+        if (trimmed[1].equals("LOCAL")) {
+            System.out.println("found local " + stack.peek().toString());
+        } else {
+            // if variable exists in first block
+            for (int i = 0; i < stack.size(); i++) {
+                if (stack.get(i).findVarialbe(trimmed[1]) != null) {
+                    System.out.println("FOUND: " + stack.get(i).findVarialbe(trimmed[1]).getName() + ":"
+                            + stack.get(i).findVarialbe(trimmed[1]).getIntegerValue());
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public static Block getAt(int index) {
+        Stack<Block> tempStack = new Stack<Block>();
+        while (!stack.empty()) {
+            tempStack.push(stack.pop());
+        }
+        for (int i = 0; i < index - 1; i++) {
+            tempStack.pop();
+        }
+        return tempStack.pop();
+    }
+
+    public static void newVariable(int startingPoint) {
+        String name = "";
+        int value = 0;
+        String trimmedLine = "";
+        String trimmed[];
+        int i = 0;
+        try {
+            // add spaces after operators
+            trimmedLine = currentLine.replace(",", " ,");
+            trimmedLine = trimmedLine.replaceAll(";", " ;");
+            trimmed = trimmedLine.split(" ");
+
+            while (!trimmed[i].equals(";")) {
+                if (trimmed[i].equals(",") || trimmed[i].equals("int")) {
+                    name = trimmed[i + 1];
+                    if (trimmed[i + 2].equals("="))
+                        value = Integer.parseInt(trimmed[i + 3]);
+                    else
+                        value = 0;
+                    stack.peek().addVariable(name, value);
+                }
+                i++;
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+            // TODO: handle exception
+        }
     }
 }
