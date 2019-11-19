563f5b8efb852d7f4a6385ee3fc50d671e14b79e
ThinPotato <bryce940@gmail.com>
2019-11-19 13:55:15 -0500

    all methods complete; may be broken

--- HomeWork6/src/app/Passage.java
+++ HomeWork6/src/app/Passage.java
@@ -9,8 +9,9 @@ class Passage {
     int wordCount;
     Hashtable<String, Double> similarTitles = new Hashtable<String, Double>();
     Hashtable<String, Integer> table = new Hashtable<String, Integer>();
-    String[] stopWords = new File("/home/bryce/Development/CSE214/HomeWork6/src/app/Stopwords.txt").toString()
-            .split("\\r?\\n");
+    String[] stopWords = new File(
+            "A:\\Users\\bryce\\Documents\\Development\\CSE214\\HomeWork6\\src\\app\\Stopwords.txt").toString()
+                    .split("\\r?\\n");
 
     public Passage(String title, File file) {
         this.title = title;
@@ -18,7 +19,22 @@ class Passage {
     }
 
     public static double cosineSimilarity(Passage passage1, Passage passage2) {
+        double top = 0;
+        double bot1 = 0;
+        double bot2 = 0;
+        String words[] = ((passage1.wordCount > passage2.wordCount) ? passage1.getWords().toArray(new String[0])
+                : passage2.getWords().toArray(new String[0]));
 
+        passage1.getWords().toArray(new String[0]);
+
+        for (int i = 0; i < words.length; i++) {
+            top += passage1.getWordFrequency(words[i]) * passage2.getWordFrequency(words[i]);
+            bot1 += (Math.pow((Double) (passage1.getWordFrequency(words[i])), 2));
+            bot2 += (Math.pow((Double) (passage2.getWordFrequency(words[i])), 2));
+        }
+        passage1.similarTitles.put(passage2.getTitle(), 100 * (top / (Math.sqrt(bot1) * Math.sqrt(bot2))));
+        passage2.similarTitles.put(passage1.getTitle(), 100 * (top / (Math.sqrt(bot1) * Math.sqrt(bot2))));
+        return 100 * (top / (Math.sqrt(bot1) * Math.sqrt(bot2)));
     }
 
     public void parseFile(File file) {
@@ -66,6 +82,6 @@ class Passage {
 
     @Override
     public String toString() {
-        return super.toString();
+        return similarTitles.toString();
     }
 }
--- HomeWork6/src/app/TextAnalyzer.java
+++ HomeWork6/src/app/TextAnalyzer.java
@@ -2,18 +2,32 @@ package app;
 
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class TextAnalyzer {
+    static ArrayList<Passage> list;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String in = "";
         System.out.println("Enter the directory of a folder of text files: ");
         in = scan.nextLine();
         File[] directoryOfFiles = new File(in).listFiles();
-        FrequencyTable mainTable = new FrequencyTable();
-
+        list = new ArrayList<Passage>();
         for (File i : directoryOfFiles) {
-            mainTable.addPassage(new Passage(i.getName(), i));
+            list.add(new Passage(i.getName(), i));
         }
+
+        for (Passage i : list) {
+            for (int k = 0; k < list.size(); k++) {
+                if (!i.title.equals(list.get(k).title))
+                    Passage.cosineSimilarity(i, list.get(k));
+            }
+        }
+
+        for (Passage i : list) {
+            System.out.println(i.toString());
+        }
+
     }
 }
