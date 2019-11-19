64220faf0d1339ee910dfcbb8d418caa188b0738
ThinPotato <bryce940@gmail.com>
2019-11-18 21:06:28 -0500

    Added most methods for Passage

--- HomeWork6/src/app/Passage.java
+++ HomeWork6/src/app/Passage.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.io.File;
+import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Set;
 
@@ -7,37 +9,57 @@ class Passage {
     String title;
     int wordCount;
     Hashtable<String, Double> similarTitles = new Hashtable<String, Double>();
+    Hashtable<String, Integer> table = new Hashtable<String, Integer>();
+    String[] stopWords = new File("/home/bryce/Development/CSE214/HomeWork6/src/app/Stopwords.txt").toString()
+            .split("\\r?\\n");
+
+    public Passage(String title, File file) {
+        this.title = title;
+        parseFile(file);
+    }
 
     public void parseFile(File file) {
         // TODO: implement method
+        String[] parsedFile = file.toString().replaceAll("[^A-Za-z0-9]", "").toLowerCase().split(" ");
+        for (int i = 0; i < parsedFile.length; i++) {
+            for (int k = 0; k < stopWords.length; k++) {
+                if (!parsedFile[i].equals(stopWords[k])) {
+                    wordCount++;
+                    if (table.get(parsedFile[i]) > 0)
+                        table.replace(parsedFile[i], table.get(parsedFile[i]) + 1);
+                    else
+                        table.put(parsedFile[i], 1);
+                }
+            }
+        }
     }
 
     public double getWordFrequency(String word) {
-        // TODO: implement method
+        return table.get(word) / wordCount;
     }
 
     public Set<String> getWords() {
-        // TODO: implement method
+        return table.keySet();
     }
 
     public String getTitle() {
-        // TODO: implement method
+        return title;
     }
 
     public int getWordCount() {
-        // TODO: implement method
+        return wordCount;
     }
 
-    public void setWordCount(int owrdCount) {
-        // TODO: implement method
+    public void setWordCount(int wordCount) {
+        this.wordCount = wordCount;
     }
 
     public Hashtable<String, Double> getSimilarTitles() {
-        // TODO: implement method
+        return similarTitles;
     }
 
     public void setSimilarTitles(Hashtable<String, Double> similarTitles) {
-        // TODO: implement method
+        this.similarTitles = similarTitles;
     }
 
     @Override
--- HomeWork6/src/app/TextAnalyzer.java
+++ HomeWork6/src/app/TextAnalyzer.java
@@ -1,5 +1,17 @@
 package app;
 
-public class TextAnalyzer {
+import java.io.File;
+import java.util.Scanner;
 
+public class TextAnalyzer {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String in = "";
+        System.out.println("Enter the directory of a folder of text files: ");
+        in = scan.nextLine();
+        File[] directoryOfFiles = new File(in).listFiles();
+        for (File i : directoryOfFiles) {
+            i.
+        }
+    }
 }
